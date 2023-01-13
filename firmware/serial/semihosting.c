e9825c01897d7d3ec99d9e39894df1b45d0e02e6
Anup Patel <apatel@ventanamicro.com>
2023-01-13 15:52:36 +0530

    firmware: serial: Sync-up semihosting implementation with OpenSBI
    We sync-up semihosting serial driver with latest OpenSBI sources.
    This also provides us semihosting puts() implementation.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

--- firmware/serial/semihosting.c
+++ firmware/serial/semihosting.c
@@ -7,93 +7,171 @@
 #include <zstage/console.h>
 #include <zstage/error.h>
 #include <zstage/serial/semihosting.h>
+#include <zstage/string.h>
 
-#define SEMIHOSTING_SYS_WRITEC		0x03
-#define SEMIHOSTING_SYS_WRITE0		0x04
-#define SEMIHOSTING_SYS_READC		0x07
-#define SEMIHOSTING_SYS_SYSERRNO	0x13
+#define SYSOPEN     0x01
+#define SYSWRITEC   0x03
+#define SYSWRITE    0x05
+#define SYSREAD     0x06
+#define SYSREADC    0x07
+#define SYSERRNO    0x13
 
-/*
- * Note: This function is intentionally noinline and 256 bytes aligned to
- * ensure that semihosting instruction sequence is within same 4K page.
- */
-static int __noinline __aligned(256) semihosting_call(int call, void *arg0)
+static long semihosting_trap(int sysnum, void *addr)
 {
-	register int ret asm ("a0") = call;
-	register void *param0 asm ("a1") = arg0;
+	register int ret asm ("a0") = sysnum;
+	register void *param0 asm ("a1") = addr;
 
 	asm volatile (
-		"\t.option push\n"
-		"\t.option norvc\n"
-		"\tslli zero, zero, 0x1f\n"
-		"\tebreak\n"
-		"\tsrai zero, zero, 7 \n"
-		"\t.option pop \n"
+		"	.align 4\n"
+		"	.option push\n"
+		"	.option norvc\n"
+
+		"	slli zero, zero, 0x1f\n"
+		"	ebreak\n"
+		"	srai zero, zero, 7\n"
+
+		"	.option pop\n"
 		: "+r" (ret) : "r" (param0) : "memory");
 
 	return ret;
 }
 
-static bool semihosting_en = true;
+static bool _semihosting_enabled = true;
 static bool try_semihosting = true;
 
 /*
  * Note: This function is intentionally noinline and 256 bytes aligned to
  * ensure that semihosting instruction sequence is within same 4K page.
  */
-bool __aligned(256) semihosting_test(void)
+bool semihosting_test(void)
 {
-	unsigned long tmp = 0;
-	register int ret asm ("a0") = SEMIHOSTING_SYS_SYSERRNO;
+	register int ret asm ("a0") = SYSERRNO;
 	register void *param0 asm ("a1") = NULL;
+	unsigned long tmp = 0;
 
 	if (!try_semihosting)
-		return semihosting_en;
+		return _semihosting_enabled;
 
 	asm volatile (
-		"\t.option push\n"
-		"\t.option norvc\n"
+		"	.align 4\n"
+		"	.option push\n"
+		"	.option norvc\n"
 
-		"\tj _semihost_test_vector_next\n"
-		"\t.align 4\n"
-		"\t_semihost_test_vector:\n"
-		"\t\tcsrr %[en], mepc\n"
-		"\t\taddi %[en], %[en], 4\n"
-		"\t\tcsrw mepc, %[en]\n"
-		"\t\tadd %[en], zero, zero\n"
-		"\t\tmret\n"
-		"\t_semihost_test_vector_next:\n"
+		"	j _semihost_test_vector_next\n"
+		"	.align 4\n"
+		"_semihost_test_vector:\n"
+		"	csrr %[en], mepc\n"
+		"	addi %[en], %[en], 4\n"
+		"	csrw mepc, %[en]\n"
+		"	add %[en], zero, zero\n"
+		"	mret\n"
+		"_semihost_test_vector_next:\n"
 
-		"\tla %[tmp], _semihost_test_vector\n"
-		"\tcsrrw %[tmp], mtvec, %[tmp]\n"
-		"\tslli zero, zero, 0x1f\n"
-		"\tebreak\n"
-		"\tsrai zero, zero, 7 \n"
-		"\tcsrw mtvec, %[tmp]\n"
+		"	la %[tmp], _semihost_test_vector\n"
+		"	csrrw %[tmp], mtvec, %[tmp]\n"
+		"	.align 4\n"
+		"	slli zero, zero, 0x1f\n"
+		"	ebreak\n"
+		"	srai zero, zero, 7\n"
+		"	csrw mtvec, %[tmp]\n"
 
-		"\t.option pop \n"
-		: [tmp] "+r" (tmp), [en] "+r" (semihosting_en),
+		"	.option pop\n"
+		: [tmp] "+r" (tmp), [en] "+r" (_semihosting_enabled),
 		  [ret] "+r" (ret)
 		: "r" (param0) : "memory");
 
 	try_semihosting = false;
-	return semihosting_en;
+	return _semihosting_enabled;
+}
+
+static int semihosting_errno(void)
+{
+	long ret = semihosting_trap(SYSERRNO, NULL);
+
+	if (ret > 0)
+		return -ret;
+	return -EIO;
+}
+
+static int semihosting_outfd = -ENODEV;
+
+static long semihosting_open(const char *fname, enum semihosting_open_mode mode)
+{
+	long fd;
+	struct semihosting_open_s {
+		const char *fname;
+		unsigned long mode;
+		size_t len;
+	} open;
+
+	open.fname = fname;
+	open.len = strlen(fname);
+	open.mode = mode;
+
+	/* Open the file on the host */
+	fd = semihosting_trap(SYSOPEN, &open);
+	if (fd == -1)
+		return semihosting_errno();
+	return fd;
+}
+
+/**
+ * struct semihosting_rdwr_s - Arguments for read and write
+ * @fd: A file descriptor returned from semihosting_open()
+ * @memp: Pointer to a buffer of memory of at least @len bytes
+ * @len: The number of bytes to read or write
+ */
+struct semihosting_rdwr_s {
+	long fd;
+	void *memp;
+	size_t len;
+};
+
+static long semihosting_write(long fd, const void *memp, size_t len)
+{
+	long ret;
+	struct semihosting_rdwr_s write;
+
+	write.fd = fd;
+	write.memp = (void *)memp;
+	write.len = len;
+
+	ret = semihosting_trap(SYSWRITE, &write);
+	if (ret < 0)
+		return semihosting_errno();
+	return len - ret;
 }
 
 static void semihosting_putc(char ch)
 {
-	semihosting_call(SEMIHOSTING_SYS_WRITEC, &ch);
+	semihosting_trap(SYSWRITEC, &ch);
+}
+
+static void semihosting_puts(const char *str, unsigned long len)
+{
+	char ch;
+	unsigned long i;
+
+	if (semihosting_outfd < 0) {
+		for (i = 0; i < len; i++) {
+			ch = str[i];
+			semihosting_trap(SYSWRITEC, &ch);
+		}
+	} else
+		semihosting_write(semihosting_outfd, str, len);
 }
 
 static struct zstage_console_device semihost_console = {
 	.name = "semihosting",
 	.console_putc = semihosting_putc,
+	.console_puts = semihosting_puts,
 };
 
 int semihosting_serial_init(void)
 {
 	if (!semihosting_test())
 		return -ENODEV;
+	semihosting_outfd = semihosting_open(":tt", MODE_WRITE);
 	zstage_console_set_device(&semihost_console);
 	return 0;
 }
--- include/zstage/serial/semihosting.h
+++ include/zstage/serial/semihosting.h
@@ -9,6 +9,29 @@
 
 #include <zstage/types.h>
 
+/**
+ * enum semihosting_open_mode - Numeric file modes for use with semihosting_open()
+ * MODE_READ: 'r'
+ * MODE_BINARY: 'b'
+ * MODE_PLUS: '+'
+ * MODE_WRITE: 'w'
+ * MODE_APPEND: 'a'
+ *
+ * These modes represent the mode string used by fopen(3) in a form which can
+ * be passed to semihosting_open(). These do NOT correspond directly to %O_RDONLY,
+ * %O_CREAT, etc; see fopen(3) for details. In particular, @MODE_PLUS
+ * effectively results in adding %O_RDWR, and @MODE_WRITE will add %O_TRUNC.
+ * For compatibility, @MODE_BINARY should be added when opening non-text files
+ * (such as images).
+ */
+enum semihosting_open_mode {
+	MODE_READ	= 0x0,
+	MODE_BINARY	= 0x1,
+	MODE_PLUS	= 0x2,
+	MODE_WRITE	= 0x4,
+	MODE_APPEND	= 0x8,
+};
+
 int semihosting_serial_init(void);
 
 #endif
